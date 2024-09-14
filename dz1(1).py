e2794447d8bdb6035d56aa53a719c296bac7651a
dianaadd <diana.valiusheva@gmail.com>
2024-09-14 18:59:10 +0300

    Add files via upload

---
+++ dz1(1).py
@@ -0,0 +1,10 @@
+import turtle
+turtle.shape("turtle")
+turtle.speed(5)
+for i in range(6):
+    turtle.circle(50)
+    turtle.left(60)
+turtle.penup()
+turtle.forward(150)
+turtle.left(90)
+turtle.done()
